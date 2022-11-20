ccf357a1400e191a30ac6c99b58fdae8527a2ca2
Ritik Patidar <ritikpatidat029@gmail.com>
2022-11-20 19:59:15 +0530

    404 page

--- client/src/pages/Page404/index.tsx
+++ client/src/pages/Page404/index.tsx
@@ -1,37 +1,56 @@
 import React from 'react';
+import { Link, useHistory } from 'react-router-dom';
 import { RoutePaths } from '../../modules/consts/enum';
-import { useSelector } from 'react-redux';
-import { getIsAuth } from '../../modules/selectors/auth';
+import './style.css';
 
 const Page404 = () => {
-    const isAuth = useSelector(getIsAuth);
-
+    const history = useHistory();
     return (
-        <div className="c-app c-default-layout flex-row align-items-center">
-            <div>
-                <div className="justify-content-center">
-                    <div>
-                        <div className="clearfix">
-                            <h1 className="float-left display-3 mr-4">404</h1>
-                            <h4 className="pt-3">Oops! You{"'"}re lost.</h4>
-                            <p className="text-muted float-left">The page you are looking for was not found.</p>
-                        </div>
-
-                        <div>
-                            <a href="/" className="btn-custom btn-primary btn-md">
-                                {isAuth ? 'Go to Home' : 'Go to Sign in'}
-                            </a>
-                            {/* <CButton
-                                to={isAuth ? RoutePaths.Dashboard : RoutePaths.Login}
-                                className="btn-custom btn-primary btn-md"
-                            >
-                                {isAuth ? 'Go to Home' : 'Go to Sign in'}
-                            </CButton> */}
-                        </div>
+        <>
+            <div className="errorBody">
+                <div className="error">
+                    <div className="wrap">
+                        
+                            <code>
+                                <span className="green">&lt;!</span>
+                                <span>DOCTYPE html</span>
+                                <span className="green">&gt;</span>
+                                <br />
+                                <span className="orange">&lt;html&gt;</span>
+                                <br />
+                                <span className="orange">&lt;style&gt;</span>
+                                <br />* {`{`}
+                                <br />
+                                <span className="green">everything</span>:<span className="blue">awesome</span>;
+                                <br />
+                                {`}`}
+                                <br />
+                                <span className="orange">&lt;/style&gt;</span>
+                                <br />
+                                <span className="orange">&lt;body&gt;</span>
+                                <br />
+                                ERROR 404! FILE NOT FOUND!
+                                <br />
+                                <span className="comment">
+                                    &lt;!--The file you are looking for, is not where you think it is.--&gt;
+                                    <br />
+                                </span>
+                                <span className="orange"></span>
+                                <br />
+                                <span className="border p-2 m-2 cursor-pointer" onClick={() => history.push('/home')} >&lt;button&gt; <b>Go to Home</b> &lt;/button&gt;</span>
+                                <br />
+                                <br />
+                                <span className="orange">&nbsp;&lt;/body&gt;</span>
+                                <br />
+                                <span className="orange">&lt;/html&gt;</span>
+                                <br />
+                            </code>
+                        
+                        <span className="info"></span>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
