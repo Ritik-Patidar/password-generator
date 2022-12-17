63f4e353196c3ebd984a069a95331ebaa4a8685b
Ritik Patidar <67598349+Ritik-Patidar@users.noreply.github.com>
2022-12-18 00:11:47 +0530

    delete unused files

--- client/src/modules/reducers/index.ts
+++ client/src/modules/reducers/index.ts
@@ -1,10 +1,8 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import authReducer from './authReducer';
-import slideShowReducer from './slideShowReducer';
 
 const reducer = combineReducers({
     auth: authReducer,
-    slideShow: slideShowReducer,
 });
 
 export type RootState = ReturnType<typeof reducer>;
