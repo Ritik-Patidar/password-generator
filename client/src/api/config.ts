6f5d36349cc2a628776800431a9e697d6562555f
Ritik Patidar <67598349+Ritik-Patidar@users.noreply.github.com>
2023-03-29 23:20:24 +0530

    added .env to frontend

--- client/src/api/config.ts
+++ client/src/api/config.ts
@@ -6,7 +6,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5555/v1/',
+    baseURL: process.env.REACT_APP_API_BASE_URL ,
     withCredentials: false,
     headers: {
         'Content-Type': 'application/json',
